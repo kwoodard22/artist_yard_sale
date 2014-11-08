bc6cfc0814d0da8e40514f4dbbaade467992cc05
kwoodard22 <kwoodard22@gmail.com>
2014-11-08 15:31:52 -0500

    Fixed photo submission

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,5 +4,5 @@ class Artist < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/gif", "image/png"] }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108150501) do
+ActiveRecord::Schema.define(version: 20141108193359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20141108150501) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "photo"
   end
 
   add_index "artists", ["email"], name: "index_artists_on_email", unique: true, using: :btree
